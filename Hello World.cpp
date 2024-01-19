d00318b9f3ad83e337acd6c07278b0a0d21bcc92
LeoG123321 <157080843+LeoG123321@users.noreply.github.com>
2024-01-18 20:02:45 -0600

    Hello World! Made
    yup yup

---
+++ Hello World.cpp
@@ -0,0 +1,18 @@
+/* 
+    Leonardo Gonzalez   1/18/2024
+
+    Hello World!
+
+    Write a program that prints “Hello World!” to the terminal and then exits gracefully.
+*/
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    cout << "Hello World!" << endl;
+
+    return 0;
+}
